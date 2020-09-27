5207fedb5db4bf10c673ca61ca0884ec6da47091
Valentin Kalinin <k-valentin-1989@yandex.ru>
2020-09-27 21:17:54 +0300

    Подключение БД и настройка Jackson.

--- src/main/java/letscode/sarafan/controller/MessageController.java
+++ src/main/java/letscode/sarafan/controller/MessageController.java
@@ -1,10 +1,14 @@
 package letscode.sarafan.controller;
 
+import com.fasterxml.jackson.annotation.JsonView;
+import letscode.sarafan.domain.Message;
+import letscode.sarafan.domain.Views;
 import letscode.sarafan.exeptions.NotFoundException;
+import letscode.sarafan.repository.MessageRepository;
+import org.springframework.beans.BeanUtils;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.HashMap;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Map;
 
@@ -12,61 +16,40 @@ import java.util.Map;
 @RequestMapping("message")
 public class MessageController {
 
-    private int counter = 4;
-    private List<Map<String, String>> messages = new ArrayList<Map<String, String>>() {{
-        add(new HashMap<String, String>() {{
-            put("id", "1");
-            put("text", "First message");
-        }});
-        add(new HashMap<String, String>() {{
-            put("id", "2");
-            put("text", "Second message");
-        }});
-        add(new HashMap<String, String>() {{
-            put("id", "3");
-            put("text", "Third message");
-        }});
-    }};
+    private final MessageRepository messageRepository;
+
+    public MessageController(MessageRepository messageRepository) {
+        this.messageRepository = messageRepository;
+    }
 
     @GetMapping
-    public List<Map<String, String>> list() {
-        return messages;
+    @JsonView(Views.IdName.class)
+    public List<Message> list() {
+        return messageRepository.findAll();
     }
 
     @GetMapping("{id}")
-    public Map<String, String> getOneMessage(@PathVariable String id) {
-        return getMessage(id);
-    }
-
-    private Map<String, String> getMessage(@PathVariable String id) {
-        return messages.stream()
-                .filter(message -> message.get("id").equals(id))
-                .findFirst()
-                .orElseThrow(NotFoundException::new);
-    }
-
-    @PostMapping
-    public Map<String, String> create(@RequestBody Map<String, String> message) {
-        message.put("id", String.valueOf(counter++));
-        messages.add(message);
+    @JsonView(Views.FullMessage.class)
+    public Message getOneMessage(@PathVariable("id") Message message) {
         return message;
     }
 
+    @PostMapping
+    public Message create(@RequestBody Message message) {
+        message.setCreationDate(LocalDateTime.now());
+        return messageRepository.save(message);
+    }
+
     @PutMapping("{id}")
-    public Map<String, String> update(@PathVariable String id,
-                                      @RequestBody Map<String, String> message) {
-        Map<String, String> messageFromDb = getMessage(id);
-
-        messageFromDb.putAll(message);
-        messageFromDb.put("id", id);
-
-        return messageFromDb;
+    public Message update(@PathVariable("id") Message messageFromDb,
+                                      @RequestBody Message message) {
+        BeanUtils.copyProperties(message, messageFromDb, "id");
+        return messageRepository.save(message);
     }
 
     @DeleteMapping("{id}")
-    public void delete(@PathVariable String id) {
-        Map<String, String> message = getMessage(id);
-        messages.remove(message);
+    public void delete(@PathVariable("id") Message message) {
+        messageRepository.delete(message);
     }
 }
 
---
+++ src/main/java/letscode/sarafan/repository/MessageRepository.java
@@ -0,0 +1,8 @@
+package letscode.sarafan.repository;
+
+import letscode.sarafan.domain.Message;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MessageRepository extends JpaRepository<Message, Long> {
+
+}
